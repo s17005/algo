bfe0869fc27d87a6753c4f1cd2948ebe1b61845e
s17005 <s17005@std.it-college.ac.jp>
2018-08-21 14:10:45 +0900

    20180815課題

---
+++ 0815/En7_1.java
@@ -0,0 +1,13 @@
+class En7_1{
+    public boolean isEmpty(){
+        return num == 0;
+    }
+
+    public boolean isFull(){
+        return num >= max;
+    }
+
+    public void clear(){
+        num = 0;
+    }
+}
