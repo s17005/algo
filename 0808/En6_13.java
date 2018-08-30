8af3e222526d0070c5089df2bfd5d1fa447fc13c
s17005 <s17005@std.it-college.ac.jp>
2018-08-30 17:01:35 +0900

    20180808課題

--- 0808/En6_13.java
+++ 0808/En6_13.java
@@ -1,50 +1,56 @@
 import java.util.Scanner;
 
 class En6_13{
-   // a[idx1]とa[idx2]を交換
-   static void swap(int[] a, int idx1, int idx2) {
-      int t = a[idx1];  a[idx1] = a[idx2];  a[idx2] = t;
-   }
 
-   // 配列を分割
-   static void quickSort(int[] a, int left, int right) {
-      int pl = left;
-      int pr = right;
-      int x = a[(pl + pr) / 2];
+  //--- 配列の要素a[idx1]とa[idx2]を交換 ---//
+  static void swap(int[] a, int idx1, int idx2) {
+    int t = a[idx1];  a[idx1] = a[idx2];  a[idx2] = t;
+  }
 
-      do {
-         while (a[pl] < x) pl++;
-         while (a[pr] > x) pr--;
-         if (pl <= pr)
-            swap(a, pl++, pr--);
-      } while (pl <= pr);
+  //--- 配列を分割する ---//
+  static void quickSort(int[] a, int left, int right) {
+    int pl = left;
+    int pr = right;
+    int x = a[(pl + pr) / 2];
 
-      if (pr - left < right - pl) {
-         int temp;
-         temp = left;  left  = pl; pl = temp;
-         temp = right; right = pr; pr = temp;
-      }
-      if (left < pr)  quickSort(a, left, pr);
-      if (pl < right) quickSort(a, pl, right);
-   }
+    System.out.printf("a[%d]～a[%d]：{", left, right);
+    for (int i = left; i < right; i++)
+      System.out.printf("%d , ", a[i]);
+    System.out.printf("%d}\n", a[right]);
 
-   public static void main(String[] args) {
-      Scanner stdIn = new Scanner(System.in);
+    do {
+      while (a[pl] < x) pl++;
+      while (a[pr] > x) pr--;
+      if (pl <= pr)
+        swap(a, pl++, pr--);
+    } while (pl <= pr);
 
-      System.out.println("クイックソート");
-      System.out.print("要素数：");
-      int nx = stdIn.nextInt();
-      int[] x = new int[nx];
+    if (pr - left < right - pl) {
+      int temp;
+      temp = left;  left  = pl; pl = temp;
+      temp = right; right = pr; pr = temp;
+    }
+    if (pl < right) quickSort(a, pl, right);
+    if (left < pr)  quickSort(a, left, pr);
+  }
 
-      for (int i = 0; i < nx; i++) {
-         System.out.print("x[" + i + "]：");
-         x[i] = stdIn.nextInt();
-      }
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
 
-      quickSort(x, 0, nx - 1);
+    System.out.println("クイックソート");
+    System.out.print("要素数：");
+    int nx = stdIn.nextInt();
+    int[] x = new int[nx];
 
-      System.out.println("昇順にソート。");
-      for (int i = 0; i < nx; i++)
-         System.out.println("x[" + i + "]＝" + x[i]);
-   }
+    for (int i = 0; i < nx; i++) {
+      System.out.print("x[" + i + "]：");
+      x[i] = stdIn.nextInt();
+    }
+
+    quickSort(x, 0, nx - 1);
+
+    System.out.println("昇順にソート");
+    for (int i = 0; i < nx; i++)
+      System.out.println("x[" + i + "]＝" + x[i]);
+  }
 }
